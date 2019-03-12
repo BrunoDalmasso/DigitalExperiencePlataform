b6e64f1c4be84126c1d434903ff2230279b8d0f4
BrunoDalmasso <bruno.dalmasso@hotmail.com>
2019-03-12 20:08:30 -0300

    Teste

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="pt" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP</title>
+    </head>
+    
+     <body>
+        <?php
+            $arrayGerado = array();
+
+            function sortearNumeros() {
+                for ($i = 0; $i <=6; $i++) {
+                    echo $i;
+                }
+                // return 1
+            }
+        ?>
+    </body>
+</html>
